7add1b1a18a4888a0f878cf3f3329752177502c3
mutua-mutinda <mutuamutinda@outlook.com>
2020-12-08 13:03:05 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search'; 
+import ShoppingBasketIcon  from '@material-ui/icons/ShoppingBasket';
+import LocationOn  from '@material-ui/icons/LocationOnOutlined';
+
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG25.png" alt=""/>
+            <LocationOn className="header__location"/>
+            <div className="header__option">
+                <span className="header__optionOne">Deliver to</span>
+                <span className="header__optionTwo">Kenya</span>
+                </div>
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+                <SearchIcon className="header__searchIcon"/>
+            </div>
+
+            <div className="header__nav">
+                <div className="header__option">
+                    <span className="header__optionOne">Hello Mutua</span>
+                    <span className="header__optionTwo">Sign In</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionOne">Returns</span>
+                    <span className="header__optionTwo">& Orders</span>
+                </div>
+
+                <div className="header__option">
+                    <span className="header__optionOne">Your</span>
+                    <span className="header__optionTwo">Prime</span>
+                </div>
+
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionTwo header__basketCount">0</span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBAjdJrGRtg95OVC7XmHdGYzziTZwQ0d7I",
+    authDomain: "clone-da72a.firebaseapp.com",
+    projectId: "clone-da72a",
+    storageBucket: "clone-da72a.appspot.com",
+    messagingSenderId: "42074569774",
+    appId: "1:42074569774:web:148d7c58cc776ad1d9f3bf",
+    measurementId: "G-76E8T2E17H"
+  };
